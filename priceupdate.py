3b4e42f8f6dafbafe2eed3f55e9dc13b159cdc41
somikdatta <developersomik@gmail.com>
2019-07-01 12:29:52 +0530

    Secure Code with User Input

--- priceupdate.py
+++ priceupdate.py
@@ -3,14 +3,23 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-URL = 'https://www.amazon.in/Sennheiser-CX-213-CX213-Earphones/dp/B00SQWD8Z8'
-
 headers = {
     "User Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
 
+# Initialization
 
-def updateprice():
+
+def main():
+    # Product URL
     URL = input()
+    fromEmail = input()
+    # Generate App password (mail) for 2FA enabled accounts, for others disable secure login from Account Settings->Security
+    fromEmailPwd = input()
+    toEmail = input()
+    updateprice(URL, fromEmail, fromEmailPwd, toEmail)
+
+
+def updateprice(URL, fromEmail, fromEmailPwd, toEmail):
     page = requests.get(URL, headers=headers)
     # If response is 200 then:
     # Parse HTML as page.content is still nascent
@@ -23,26 +32,24 @@ def updateprice():
     priceNum = float(price[2:7].replace(',', '.'))
     # Email update if price falls
     if(priceNum <= 1.0):
-        mailer(title, priceNum)
+        mailer(URL, title, priceNum, fromEmail, fromEmailPwd, toEmail)
 
 # mailer() method uses Gmail's SMTP
 
 
-def mailer(title, currentPrice):
+def mailer(URL, title, currentPrice, fromEmail, fromEmailPwd, toEmail):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    # Generate App password (mail) for 2FA enabled accounts, for others disable secure login from Account Settings->Security
-    server.login('from-emailid@example.com', 'examplepassword')
+    server.login(fromEmail, fromEmailPwd)
     subject = f'Prices Down for {title}'
     body = f'Check the Amazon link: {URL}\nCurrent Price: Rs.{currentPrice}'
     message = f'Subject: {subject}\n\n{body}'
-    server.sendmail('from-emailid@example.com',
-                    'to-emailid@example.com', message)
+    server.sendmail(fromEmail,
+                    toEmail, message)
     server.quit()
 
 
-while(True):
-    updateprice()
-    time.sleep(60*60*12)
+if __name__ == "__main__":
+    main()
